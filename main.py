7908df69cac506b42e6bb5527d566263a13c7c93
DLong84 <dlon121@wgu.edu>
2024-01-07 21:03:15 -0500

    feat(): address calculations
    -updated distanceBtwn() as "overloaded" to take in the addresses as strings or integers
    -implemented nearestAddress() for obtaining the address closest to the current address
    -fixed bug/typo in delivery_addresses.csv
    -ALL updates and implementations have been successfully tested
    
    TODO's & FIXME's in place

--- main.py
+++ main.py
@@ -3,3 +3,6 @@
 from package import Package
 from hash_tbl import HashTable
 
+pckgHashTbl = HashTable()
+Package.loadPackageInfo("CSV/packages.csv", pckgHashTbl)  # LOAD Package data into hash table
+pckgHashTbl.printAllPkgs()
